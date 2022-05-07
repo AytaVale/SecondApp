0591b10c231c1393e6948017696e5f4d369cfb88
Aitaj <aitaj@example.com>
2022-05-07 22:15:22 +0400

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+Welcome:
+
+Console.Write("Yashinizi qeyd edin: ");
+int age = Convert.ToInt32(System.Console.ReadLine());
+
+if (age > 18)
+{
+    Console.Write("Balinizi qeyd edin: ");
+    int point = Convert.ToInt32(Console.ReadLine());
+    if (point>50)
+    {
+        Console.WriteLine("Tebrikler");
+    }
+    else
+    {
+        goto Welcome;
+    }
+}
+else {
+    goto Welcome;
+}
